07ddfbef5e196419234f9560643792233baa19da
Ahmed <94252954+b1ahmed8@users.noreply.github.com>
2022-09-12 15:50:29 +0100

    chat bot

---
+++ chatbot.py
@@ -0,0 +1,14 @@
+user_feeling = input("How are you feeling? ")
+
+good_emotions = ["ok","good","amazing","great","happy"]
+
+good = False
+
+for emotion in good_emotions:
+    if emotion in user_feeling:
+        print("thats great!")
+        good = True
+        break
+
+if not good:
+    print("oh")
